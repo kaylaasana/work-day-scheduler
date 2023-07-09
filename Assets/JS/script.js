fc37e1bd069a2d887b6f75f91ccda1ec96f39abd
kayla <you@example.com>
2023-07-08 17:50:38 -0700

    added classes to time blocks based on current time of day

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -29,28 +29,23 @@ $(function () {
         //figure out what hour block you are in by using $(this);
         //key name needs to match the key names for getItem localStorage
         //setItem for localStorage to save the input of only that particular hour by using 'this'
-  // });
+  // });  
 
-  //First
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  // 0. use jquery.children to select the parent element of the child we want to conver into array (time blocks)
+  //grab current time (just the hour)
+  var currentHour = dayjs().hour();
 
-  // document.querySelectorAll("time-block"); < -- use this method in jquery to select entire class to target an array of the time blocks in for loop described below vv
-
-  // 1. use dayjs() to grab current time (just the hour)
-  // 2. use for loop for all the time blocks 
-        // 0. inside the for loop select the time block
-        //start for loop with i = 9 rather than 0; i < 18; i++
-        // document.querySelector('#hour-' + i); < -- jquery version
-        //a. use if statement to compare the current hour to the hour of the block we are on  (ex: compare to 9a then 10a then 11a etc etc)
-        // b. if the current hour is less than time block apple future class
-        // c. else if the current time is equal to time block apply present class
-        // d. else the current time is greater than time block apply past class
-  // 3. remove past, present and future class in html and append the correct class       
+  for (var i = 9; i < 18; i++) {   
+      var hourEl = $('#hour-' + i);
+      
+      if (currentHour < i) {
+            hourEl.addClass('present');
+      }else if (currentHour === i) {
+            hourEl.addClass('past');
+      }else {
+            hourEl.addClass('future');
+      }
+      
+};       
 
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
@@ -66,5 +61,7 @@ $(function () {
 
   // 1. create a variable and use dayjs() to get current date (time optional)
   // 2. $('#currentDay).text(var)
+
+ 
 
 });
