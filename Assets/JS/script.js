f07656834fe2d0dd2054379cf62691684b87a028
kayla <you@example.com>
2023-07-09 16:38:03 -0700

    added set interval function to update date and time on page, begin README

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,28 +1,36 @@
-//define current date and display it on the page
-var currentDate = dayjs().format("dddd, " + "MMMM, " + "D " + "YYYY");
-$("#currentDate").append(currentDate);
-
-//define time and display on page
-var currentTime = dayjs().format("h:" + "mm" + " A");
-$("#currentTime").append(currentTime).css("font-size", "25px");
-
 $(function () {
+  //function to set up updating time display
+  function refreshTime(){
+    //define time and display on page
+    var currentTime = dayjs().format("h:mm A");
+    $("#currentTime").text(currentTime).css("font-size", "25px");
+    //define current date and display it on the page
+    var currentDate = dayjs().format("dddd, MMMM, D YYYY");
+    $("#currentDate").text(currentDate);
+  }
+  refreshTime();
+  //set interval to update every one second to give a relatively accurate time and date on page
+  setInterval(refreshTime, 1000);
   //check local storage for saved data
   $(".description").val(function () {
     var hourSaved = $(this).closest(".time-block").attr("id");
     return localStorage.getItem(hourSaved);
   });
+
   //event listener that listens for clicks on all save buttons and saves text data to local storage
   $(".saveBtn").on("click", function () {
     var hourSaved = $(this).closest(".time-block").attr("id");
     var savedEvent = $(this).siblings(".description").val();
     localStorage.setItem(hourSaved, savedEvent);
+
+    
   });
 
-  //grab current time (just the hour)
-  var currentHour = dayjs().hour();
+  
   //applies styling from CSS using classes based on the current hour
   for (var i = 9; i < 18; i++) {
+    //grab current time (just the hour)
+    var currentHour = dayjs().hour();
     var hourEl = $("#hour-" + i);
 
     if (currentHour > i) {
@@ -34,5 +42,5 @@ $(function () {
     }
   }
 
-  //create function that checks time every 1 minute and updates the time on the page. look up setInterval
+  // maybe create function that checks time every 1 minute and updates the time on the page. look up setInterval
 });
--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# work-day-scheduler
+## Work Day Scheduler
+
+## Description
+
+Provide a short description explaining the what, why, and how of your project. Use the following questions as a guide:
+
+- What was your motivation?
+- Why did you build this project? (Note: the answer is not "Because it was a homework assignment.")
+- What problem does it solve?
+- What did you learn?
+
+## Table of Contents (Optional)
+
+If your README is long, add a table of contents to make it easy for users to find what they need.
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+## Usage
+
+Provide instructions and examples for use. Include screenshots as needed.
+
+To add a screenshot, create an `assets/images` folder in your repository and upload your screenshot to it. Then, using the relative filepath, add it to your README using the following syntax:
+
+    ```md
+    ![alt text](assets/images/screenshot.png)
+    ```
+
+## Credits
+
+Starter code provided by UC Berkeley Extension program.
+
+## Live Link
+
